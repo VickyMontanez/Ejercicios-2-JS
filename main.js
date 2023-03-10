ba861b4ce5ed35007c9fb4a908bb602fdf2f0dfc
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-10 07:49:10 -0500

    feat: :tada: Config workspace
    Exercises of JavaScript

---
+++ main.js
@@ -0,0 +1,4 @@
+// Taller JavaScript 2
+//Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, de lo contrario un mensaje que diga "becado"
+
+
