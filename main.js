145399a1f76829c4ba9f89a53dba651e56a58e0b
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-03-12 20:14:11 -0500

    feat: :sparkles: Fifth exercise
    Build an algorithm that entry 2 numbers and if the first number is greater than the second one, the algorithm returns me an addition and a subtraction or if the first number is less than the second one, the algorithm returns me a multiplication and a division.

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
 // Taller JavaScript 2
-//4. Construir el algoritmo que solicite el nombre y edad de 3 personas y determine el nombre de la persona con mayor edad.
+//5. Construir el algoritmo que lea por teclado dos números, si el primero es mayor al segundo informar su suma y diferencia, en caso contrario, informar el producto y la división del primero respecto al segundo.
 
-let nombres= [];
-let edades = [];
-let usu = {Nombre: nombres, Edad :edades};
-let indice;
-let mayor;
+let numeros =[]; 
 
-for (let i = 1; i <4; i++){
-    nombres.push(prompt(`Ingresa ${i} el nombre:`));
-    edades.push(Number(prompt(`Ingresa ${i} la edad:`)));
+for (let i = 0; i <2; i++){
+     numeros.push(Number(prompt(`Ingresa ${i+1} número:`)));
 }
 
-indice = edades.indexOf(Math.max(...edades));
-mayor = Math.max(...edades);
-console.table(usu);
-console.log("La persona con mayor edad es",nombres[indice],`con ${mayor} años`);
+mayorn= Math.max(...numeros)
+mayori = numeros.indexOf(Math.max(...numeros));
+
+if (mayori==0){
+    function add(arrNumbers) {
+        var resultado = arrNumbers.reduce(function(a, b) {
+          return a + b;
+        });
+        return resultado;
+      }
+
+      function sub(arrNumbers) {
+        var resultado = arrNumbers.reduce(function(a, b) {
+          return a - b;
+        });
+        return resultado;
+      }
+    console.log(`Tus números son ${numeros}`);
+    console.log(`El primer número ${mayorn} es el mayor entonces: `);
+    console.log("La suma  es: " + add(numeros));
+    console.log("La resta es: " + sub(numeros));
+} else if (mayori == 1){
+    function mul(arrNumbers) {
+        var resultado = arrNumbers.reduce(function(a, b) {
+          return a * b;
+        });
+        return resultado;
+    }
+    function div(arrNumbers) {
+        var resultado = arrNumbers.reduce(function(a, b) {
+          return a / b;
+        });
+        return resultado;
+    }
+    console.log(`Tus números son ${numeros}`);
+    console.log(`El segundo número ${mayorn} es el mayor entonces: `);
+    console.log("La multiplicación es: " + mul(numeros));
+    console.log("La division es: " + div(numeros));
+}
