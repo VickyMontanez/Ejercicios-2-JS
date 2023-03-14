869de3fbd0f2a886ba495a6183f89b18d374b13c
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-14 06:50:22 -0500

    feat: :sparkles: Seventh exercise
    Build an algorithm that entry any quantity of products with its price and its amount, and returns  me  the total to pay.

--- main.js
+++ main.js
@@ -1,30 +1,22 @@
 // Taller JavaScript 2
-//6.Construir el algoritmo en Javascript para un programa para cualquier cantidad de estudiantes que lea el nombre, el sexo y la nota definitiva y halle al estudiante con la mayor nota y al estudiante con la menor nota y cuantos eran hombres y cuantos mujeres.
+// 7.Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura.
 
-let nombres = [];
-let sexo = [];
-let notas = [];
-let alumnos= {Alumno:nombres, Sexo:sexo, Nota:notas};
+let articulos = [];
+let precio = [];
+let cantidad = [];
+let productos = {Producto:articulos, Precio: precio, Cantidad: cantidad}
+let resultado = 0;
 
-function myFunction() {
-    let text = "Deseas agregar un alumno?";
+function myFactura() {
+    let text = "¿Deseas agregar algún articulo?";
     while (confirm(text) == true) {
-        nombres.push(prompt("Agrega el nombre del alumno:"));
-        sexo.push(prompt("Agrega el sexo del alumno (F/M):"));
-        notas.push(Number(prompt("Agrega la nota definitiva del alumno:")))
+        articulos.push(prompt("¿Cuál es el nombre del producto?"));
+        precio.push(Number(prompt("¿Cuál es el precio del articulo?")));
+        cantidad.push(Number(prompt("¿Cuál es la cantidad?")))
     }
-
-    let mayori = notas.indexOf(Math.max(...notas));
-    let mayorn = Math.max(...notas);
-    let menori = notas.indexOf(Math.min(...notas));
-    let menorn = Math.min(...notas);
-    
-    var repetidos = {};
-    sexo.forEach(function(numero){
-        repetidos[numero] = (repetidos[numero] || 0) + 1;});
-
-    console.table(alumnos);
-    console.log(`El estudiante ${nombres[mayori]} tiene la MAYOR nota : ${mayorn}`);
-    console.log(`El estudiante ${nombres[menori]} tiene la MENOR nota : ${menorn}`);
-    console.log(`En esta clase hay tantos estudiantes (MASCULINOS/FEMENINOS):`, repetidos);
-}
+    for (let i = 0; i < articulos.length; i++) {
+        resultado = (precio[i]*cantidad[i])+resultado;
+    }
+    console.table(productos);
+    console.log("Su total a pagar es ", resultado);
+} 
